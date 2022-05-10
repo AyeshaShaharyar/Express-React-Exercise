a0fc5bc54b3543e61bf63551063818afe0fe3bff
AyeshaShaharyar <ayesha2598@gmail.com>
2022-05-10 15:01:13 -0400

    add even listner to btns

--- web/src/components/Button.js
+++ web/src/components/Button.js
@@ -1,24 +1,25 @@
 import React from 'react';
 
-export default function Button(props) {
-  function handleBtn(event) {
-    console.log(event.target.value);
+export default function Button() {
+  function handleBtn(e) {
+    console.log(e.target.value);
   }
+
   return (
     <div>
-      <button value="All" onClick={() => handleBtn(props.handleBtn)}>
+      <button value="All" onClick={(e) => handleBtn(e)}>
         All
       </button>
-      <button value="Typescript" onClick={() => handleBtn(props.handleBtn)}>
+      <button value="Typescript" onClick={(e) => handleBtn(e)}>
         Typescript
       </button>
-      <button value="English" onClick={() => handleBtn(props.handleBtn)}>
+      <button value="English" onClick={(e) => handleBtn(e)}>
         English
       </button>
-      <button value="French" onClick={() => handleBtn(props.handleBtn)}>
+      <button value="French" onClick={(e) => handleBtn(e)}>
         French
       </button>
-      <button value="PHP" onClick={() => handleBtn(props.handleBtn)}>
+      <button value="PHP" onClick={(e) => handleBtn(e)}>
         PHP
       </button>
     </div>

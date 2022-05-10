c6d165ce493470b5f7472f72dc3b68e5c7e68fd2
AyeshaShaharyar <ayesha2598@gmail.com>
2022-05-09 23:47:32 -0400

    fetch data from api

---
+++ web/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import List from './components/List';
+
+import './App.css';
+
+export function App() {
+  return (
+    <div className="App">
+      <List />
+    </div>
+  );
+}
---
+++ web/src/components/List.js
@@ -0,0 +1,43 @@
+import { React, useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function List() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const URL = `http://localhost:4000/repos`;
+    try {
+      axios.get(URL).then((response) => {
+        setList(response.data);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>list of repositories </h1>
+      <table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Language</th>
+            <th>Forks</th>
+          </tr>
+          {list.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{item.name}</td>
+                <td>{item.description}</td>
+                <td>{item.language}</td>
+                <td>{item.forks_count}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}

2f31f070ea37a292138cbbe19eec34558a0c386a
AyeshaShaharyar <ayesha2598@gmail.com>
2022-05-10 00:54:05 -0400

    add styling to list of repo

--- web/src/components/List.js
+++ web/src/components/List.js
@@ -21,8 +21,8 @@ export default function List() {
 
   return (
     <div>
-      <h1>list of repositories </h1>
-      <table>
+      <h1 className="heading">list of repositories </h1>
+      <table className="table">
         <tbody>
           <tr>
             <th>Name</th>

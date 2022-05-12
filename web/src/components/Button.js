9e3799a54d9183d24611dc86948df3213d0411c6
AyeshaShaharyar <ayesha2598@gmail.com>
2022-05-11 21:34:41 -0400

    add style to button and body

--- web/src/components/Button.js
+++ web/src/components/Button.js
@@ -1,6 +1,7 @@
 import { React, useState, useEffect } from 'react';
 import axios from 'axios';
 import List from './List';
+import '../App.css';
 
 export default function Button() {
   //get data from api
